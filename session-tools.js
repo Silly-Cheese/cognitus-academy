32a32e5bac0f62c4fed0547fa12854d3657762e1
Executive_Eagle <christophershelley257@gmail.com>
2026-06-28 16:25:25 -0500

    Add remember me checkbox

--- session-tools.js
+++ session-tools.js
@@ -1,1 +1,6 @@
-console.log('session tools loaded');
+function academySessionTools(){
+  const btn = document.getElementById('loginBtn');
+  if(!btn || document.getElementById('rememberSession')) return;
+  btn.insertAdjacentHTML('beforebegin','<label class="field"><span><input id="rememberSession" type="checkbox" checked style="width:auto;margin-right:8px">Keep me signed in</span></label>');
+}
+setInterval(academySessionTools, 500);
