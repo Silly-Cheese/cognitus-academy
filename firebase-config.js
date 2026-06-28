a6a562b3742f308d1c2ba50abafdf1ccc9527331
Executive_Eagle <christophershelley257@gmail.com>
2026-06-28 13:15:48 -0500

    Add local Firebase config placeholder

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Replace the placeholder values with your Firebase web app configuration.
+// Keep this file private if you do not want project details in a public repo.
+export const firebaseConfig = {
+  apiKey: 'PASTE_FIREBASE_API_KEY_HERE',
+  authDomain: 'cognitus-academy.firebaseapp.com',
+  projectId: 'cognitus-academy',
+  storageBucket: 'cognitus-academy.firebasestorage.app',
+  messagingSenderId: '437707516587',
+  appId: '1:437707516587:web:a754ae751ee42d8040e5ee'
+};
