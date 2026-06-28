f8ead95d5f27a2a5f34a204d7980bc9f76ccb565
Executive_Eagle <christophershelley257@gmail.com>
2026-06-28 13:18:41 -0500

    Update Firebase configuration with new API key

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,8 @@
-// Replace the placeholder values with your Firebase web app configuration.
-// Keep this file private if you do not want project details in a public repo.
 export const firebaseConfig = {
-  apiKey: 'PASTE_FIREBASE_API_KEY_HERE',
-  authDomain: 'cognitus-academy.firebaseapp.com',
-  projectId: 'cognitus-academy',
-  storageBucket: 'cognitus-academy.firebasestorage.app',
-  messagingSenderId: '437707516587',
-  appId: '1:437707516587:web:a754ae751ee42d8040e5ee'
+  apiKey: "AIzaSyCacVZigbcAl-ukzQRfHB6DEt6iwnMExXY",
+  authDomain: "cognitus-academy.firebaseapp.com",
+  projectId: "cognitus-academy",
+  storageBucket: "cognitus-academy.firebasestorage.app",
+  messagingSenderId: "437707516587",
+  appId: "1:437707516587:web:a754ae751ee42d8040e5ee"
 };
