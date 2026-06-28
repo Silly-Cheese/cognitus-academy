9a469490d734ad73f45bfc20e43cbb2b58b38321
Executive_Eagle <christophershelley257@gmail.com>
2026-06-28 14:01:31 -0500

    Add Firebase configuration to firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,8 @@
-
+export const firebaseConfig = {
+  apiKey: "AIzaSyCacVZigbcAl-ukzQRfHB6DEt6iwnMExXY",
+  authDomain: "cognitus-academy.firebaseapp.com",
+  projectId: "cognitus-academy",
+  storageBucket: "cognitus-academy.firebasestorage.app",
+  messagingSenderId: "437707516587",
+  appId: "1:437707516587:web:a754ae751ee42d8040e5ee"
+};
