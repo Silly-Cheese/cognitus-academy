1394f74bce310bebdf573959e35916d02864c516
Executive_Eagle <christophershelley257@gmail.com>
2026-06-28 13:09:14 -0500

    Add Firebase configuration example

---
+++ firebase-config.example.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "PASTE_FIREBASE_API_KEY_HERE",
+  authDomain: "cognitus-academy.firebaseapp.com",
+  projectId: "cognitus-academy",
+  storageBucket: "cognitus-academy.firebasestorage.app",
+  messagingSenderId: "437707516587",
+  appId: "1:437707516587:web:a754ae751ee42d8040e5ee"
+};
